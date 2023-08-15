0237e6706fb52d31296c4900bfa2061c884ffba7
umeshjain1999 <ujain2601@gmail.com>
2023-08-15 21:59:37 +0530

    fix: add drag

--- components/modal.tsx
+++ components/modal.tsx
@@ -19,14 +19,6 @@ const Modal = (props: {
 
   return (
     <motion.div
-      onPan={(e, info) => y.set(info.offset.y)}
-      onPanEnd={(e, info) => {
-        if (info.offset.y < -80) {
-          y.set(-280);
-        } else {
-          y.set(0);
-        }
-      }}
       style={{ height }}
       key="modal"
       initial={{
@@ -45,12 +37,25 @@ const Modal = (props: {
       }}
       className="absolute bottom-0 left-0 w-full overflow-auto bg-zinc-50 rounded-2xl no-scrollbar shadow-md shadow-zinc-200 dark:shadow-zinc-100"
     >
-      <div className="flex items-center justify-between p-4 bg-zinc-50 dark:bg-zinc-100 sticky top-0 after:content-[''] after:absolute after:top-1 after:left-[50%] after:translate-x-[-50%] after:w-8 after:h-1 after:rounded-2xl after:bg-zinc-400">
+      <motion.div
+        onPan={(e, info) => y.set(info.offset.y)}
+        onPanEnd={(e, info) => {
+          if (info.offset.y < -50) {
+            y.set(-280);
+          } else {
+            y.set(0);
+          }
+        }}
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={0}
+        className="flex items-center justify-between p-4 bg-zinc-50 dark:bg-zinc-100 sticky top-0 after:content-[''] after:absolute after:top-1 after:left-[50%] after:translate-x-[-50%] after:w-8 after:h-1 after:rounded-2xl after:bg-zinc-400"
+      >
         <div className="font-bold text-xl text-zinc-900">{props.title}</div>
         <span onClick={props.handleModalClick} className="cursor-pointer">
           <CloseIcon />
         </span>
-      </div>
+      </motion.div>
       <div className="p-4">
         <div
           className="prose prose-sm"
